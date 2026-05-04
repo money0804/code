03cd62f9e3312fa55b990c48ffca96cbfe2091d7
money0804 <money08040804@gmail.com>
2026-05-05 00:02:18 +0800

    Update 390E.cpp

--- AtCoder/390E.cpp
+++ AtCoder/390E.cpp
@@ -1,12 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a[6000][3];
-int main(){
+const int MOD = 1e9 + 7;
+#define ll long long
+#define int long long
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mem(a, v) memset(a, v, sizeof(a))
+#define pb push_back
+#define all(_v) _v.begin(), _v.end()
+const ll INF = 1e18;
+const int MAXN = 1e6+5;
+int a[6000][4];
+int dp[5][30000000];
+signed main(){
 	int n,x;
 	cin>>n>>x;
 	for(int i=1;i<=n;i++){
-		for(int j=0;j<3;j++){
-		cin>>a[i][j];
+        int b,c,d;
+        cin>>b>>c>>d;
+        for(int i=x;i>=d;i--){
+            dp[b][i]=max(dp[b][i],dp[b][i-d]+c);
+        }
 		}
-	}
+int te=0,ans;
+    for(int i=0;i<=x;i++){
+        for(int j=0;j<=x-i;j++){
+            int c=x-i-j;
+            ans=min(min(dp[1][i],dp[2][j]),dp[3][c]);
+            //cout<<ans<<'\n';
+            te=max(te,ans);
+        }
+    }
+    cout<<te;
 }
