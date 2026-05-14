2b6e20b3ce474061d7e8cdf756135cf8f6864931
money0804 <money08040804@gmail.com>
2026-05-14 18:01:33 +0800

    Update and rename 2164 to 2164.cpp

---
+++ CSES/2164.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	int a,b;
+	cin>>a>>b;
+	int pos=0;
+	for(int i=0;i<b;i++){
+	
+	}
+	cout<<pos+1<<'\n';
+	}
+}
