2c499d9464ff3b0f7515360d42f499a30c042703
money0804 <money08040804@gmail.com>
2026-05-21 16:32:13 +0800

    Update 2164.cpp

--- CSES/2164.cpp
+++ CSES/2164.cpp
@@ -1,16 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9 + 7;
+#define ll long long
+//#define int long long
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mem(a, v) memset(a, v, sizeof(a))
+#define pb push_back
+#define all(_v) _v.begin(), _v.end()
+const ll INF = 1e18;
+const int MAXN = 1e6+5;
 
-int main(){
-	int t;
-	cin>>t;
-	while(t--){
-	int a,b;
-	cin>>a>>b;
-	int pos=0;
-	for(int i=0;i<b;i++){
-	
-	}
-	cout<<pos+1<<'\n';
-	}
+int sol(int n,int k){
+    if(n==1) return 1;
+    if(k<=float(n/2)) return 2*k;
+
+}
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k
+        cin>>n>>k;
+
+    } 
+
 }
